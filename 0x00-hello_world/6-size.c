ee980165df2216d0099ee2e63012ecab6a43896f
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-01 18:29:33 +0000

    Betty style listing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,10 @@
  */
 int main(void)
 {
-    char c;
-    int i;
-    long int li;
-    long long int lli;
-    float d;
-
-    printf("Size of a char: %d byte(s)\n", size of (c));
-    printf("Size of an int: %d byte(s)\n", size of (i));
-    printf("Size of a long int: %d byte(s)\n", size of (li));
-    printf("Size of a long long int: %d byte(s)\n", size of (lli);
-    printf("Size of a float: %d byte(s)\n", size of (d));
+    printf("Size of a char: %d byte(s)\n", sizeof (c));
+    printf("Size of an int: %d byte(s)\n", sizeof (i));
+    printf("Size of a long int: %d byte(s)\n", sizeof (li));
+    printf("Size of a long long int: %d byte(s)\n", sizeof (lli);
+    printf("Size of a float: %d byte(s)\n", sizeof (d));
     return (0)
 }
