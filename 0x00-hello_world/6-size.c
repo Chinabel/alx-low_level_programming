c5d34952cda28143ba3b0793a2e6c4c625ceae42
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-01 19:12:47 +0000

    Betty style listing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
- char charType;
- int intType;
- long int longIntType;
- long long int longLongIntType;
- float floatType;
+char charType; 
+int intType;
+long int longIntType; 
+long long int longLongIntType;
+float floatType;
 
- printf("Size of a char: %zu bytes\n", sizeof(charType));
- printf("Size of an int: %zu bytes\n", sizeof(intType));
- printf("Size of a long int: %zu bytes\n", sizeof(longIntType));
- printf("Size of a long long int: %zu bytes\n", sizeof(longLongIntType));
- printf("Size of a float: %zu bytes\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
- return (0);
+return (0);
 }
