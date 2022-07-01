1cd4f17b8166f603cd0e231b4f823d3ba48194ba
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-01 19:47:58 +0000

    printing a new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
-scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+scanf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
