f6c27ba8ffa17c4f95ed4918a6ad4b6e02eb9f2c
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-01 18:23:27 +0000

    Betty style listing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,18 +3,18 @@
  * main - Entry point
  * Return: Always 0 (success)
  */
-int main()
+int main(void)
 {
-    int i;
-    long int l;
-    long long int ll;
-    float d;
     char c;
+    int i;
+    long int li;
+    long long int lli;
+    float d;
 
-    printf("Size of a char: %lubyte(s)\n", size of (c));
-    printf("Size of an int: %lubyte(s)\n", size of (i));
-    printf("Size of a long int: %lubyte(s)\n", size of (l));
-    printf("Size of a long long int: %lubyte(s)\n", size of (ll);
-    printf("Size of a float: %lubyte(s)\n", size of (d));
+    printf("Size of a char: %d byte(s)\n", size of (c));
+    printf("Size of an int: %d byte(s)\n", size of (i));
+    printf("Size of a long int: %d byte(s)\n", size of (li));
+    printf("Size of a long long int: %d byte(s)\n", size of (lli);
+    printf("Size of a float: %d byte(s)\n", size of (d));
     return (0)
 }
