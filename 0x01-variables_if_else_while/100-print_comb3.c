14c2e00f54e30a7fbd1308699bab9c4e92fb5332
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-02 11:24:08 +0000

    print different combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,7 @@ for (num1 = 0; num1 < 9; num1++)
 for (num2 = 0; num2 < 10; num2++)
 {
 
-putchar((num1 % 10) + '0');
+putchar((num1 % 9) + '0');
 putchar((num2 % 10) + '0');
 
 if (num1 == 8 && num2 == 9)
