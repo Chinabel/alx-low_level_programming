2991f592db3d88d885863b7d7e0206932ff5d70f
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-02 06:50:16 +0000

    print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print single numbers in base 10 beginning from 0
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+
+for (n = '0')
+
+if (n <= '9')
+{
+putchar(n)
+n++;
+}
+
+putchar('\n\n');
+
+return (0);
+}
