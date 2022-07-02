301003e49af1709822761e129c5aa3703b1a3eb3
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-02 09:07:56 +0000

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print all base 16 numbers in lowercase
+ * followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+char ch;
+
+for (n = 0; n <= 10; n++)
+{
+putchar((n % 10) + '0');
+}
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+
+putchar('\n');
+
+return (0);
+} 
