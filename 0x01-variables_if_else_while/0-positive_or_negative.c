e287fe1ecdb143d1e2b5ecbad55af5b7f2792555
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-02 04:24:02 +0000

    if else statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,16 @@ int main(void)
 {
 int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if n >= 0;
-           n is positive
-        else if n == 0;
-           n is zero
-        else if n <= 0;
-           n is negative
-        else;
-           n = rand()
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0);
+  n is positive
+else if n = 0;
+  n is zero
+else if n < 0;
+  n is negative
+else;
+  n = rand()
+
+return (0);
 }
