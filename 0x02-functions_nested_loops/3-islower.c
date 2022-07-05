136ff4effc3c2847b37f8b6a61843ffb305511d4
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-05 21:33:23 +0000

    character is in lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * int_islower - check for lowercase character
+ * Return: 1 if c is lowercase
+ * Return: 0 if otherwise
+ */
+
+int _islower(int c)
+{
+char ch;
+
+if (islower(ch))
+{
+putchar("'%c' is a lowercase alphabet.", ch);
+}
+
+else
+
+return (0);
+}
