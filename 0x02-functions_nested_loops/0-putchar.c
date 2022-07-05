a85334f76f7e6d5efbd94d6b08ba0218afb1c410
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-05 13:35:51 +0000

    putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
-#if MAIN
- # include "main.h"
-#endif
+#include <main.h>
+#include <stdlib.h>
 
 /**
  * main - prints putchar() followed by a new line
  * Return: 0 if success
  **/
 
-int main(void)
+int main()
 {
 int _putchar = ch;
 
