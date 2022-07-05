15fb1f753c6d086bf9f9084fc81684af10a6583f
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-05 13:23:46 +0000

    putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - prints putchar() followed by a new line
+ * Return: 0 if success
+ **/
+
+int main(void)
+{
+int _putchar = ch;
+
+putchar("%c", ch); 
+}
+
+putchar('\n');
+
+return (0);
+}
