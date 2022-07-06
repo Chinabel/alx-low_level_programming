b5bc8eaeca1d163e88514a2720cb2fa56c3604bc
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-06 01:24:56 +0000

    what is the value of n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
- 
+
 /**
  * print_sign - prints the sign of a number
  * @n: the integer we will use in this function
@@ -9,10 +9,10 @@
 
 int print_sign(int n)
 {
-if (n > 0)                                              
+if (n > 0)
 {
 _putchar('+');
-                                                           
+
 return (1);
 }
 else if (n < 0)
