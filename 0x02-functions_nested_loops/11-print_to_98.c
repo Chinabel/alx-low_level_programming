7857e824f9a58de13590f93c5a90dcc575c66961
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-06 03:45:10 +0000

    these are natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * followed by a new line
+ * Numbers must be separated by a comma followed by a space
+ *
+ * Return: Always 0
+ **/
+
+void print_to_98(int n)
+{
+printf("The natural numbers between n and 98 are:\n");
+
+for (n = 1; n <= 98; n++)
+{
+printf("%d", n);
+}
+printf('\n');
+
+return (0);
+}
