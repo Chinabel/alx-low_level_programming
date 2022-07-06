cdc89b86b2494521ee2a3245c1cfd3c5e7870631
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-06 09:32:37 +0000

    print all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,18 +15,18 @@ void print_to_98(int n)
 if (n >= 98)
 {
 while (n > 98)
-printf("%d", n++);
+printf("%d", n--);
 printf("%d\n", n);
-printf(',');
-printf(' ');
+_putchar(',');
+_putchar(' ');
 }
 
 else
 {
 while (n < 98)
-printf("%d", n--);
+printf("%d", n++);
 printf("%d\n", n);
-printf(',');
-printf(' ');
+_putchar(',');
+_putchar(' ');
 }
 }
