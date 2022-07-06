79414c5b757d8cfc426aa01afe75ac78a9cc9a89
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-06 00:36:27 +0000

    what is the value of n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the integer we will use in this function
+ * Return: 0 if equals to zero
+ **/
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+
+result (1);
+
+}
+
+else if (n < 0)
+{
+
+result (-1);
+
+}
+
+else
+
+result (0);
+
+}
