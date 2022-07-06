ddedc733c2b3483899398417bde05536d5dff0b8
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-06 00:14:18 +0000

    search for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the integer we will use for this function
+ * Return: 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+int k;
+
+if (k >= 'a' && k<= 'z') || (k >= 'K' && k <= 'Z')
+{
+result (1);
+}
+else
+
+result (0);
+}
