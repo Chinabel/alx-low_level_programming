c84fdce6eee50ea6a5f937bcd184dff795612b95
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-06 00:43:06 +0000

    what is the value of n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,24 +12,19 @@ int print_sign(int n)
 
 if (n > 0)
 {
-
 _putchar('+');
 result (1);
-
 }
 
 else if (n < 0)
 {
-
 _putchar('-');
 result (-1);
-
 }
 
 else
-
+{
 _putchar('0');
-
 result (0);
-
+}
 }
