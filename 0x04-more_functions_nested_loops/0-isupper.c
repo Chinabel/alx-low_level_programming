387205afef358dee3c0a45a44f04e74583e5c57f
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-07 15:30:38 +0000

    Uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+int c;
+
+if (c = 'A'; c <= 'Z'; c++)
+{
+_putchar(c);
+}
+
+return (1);
+}
