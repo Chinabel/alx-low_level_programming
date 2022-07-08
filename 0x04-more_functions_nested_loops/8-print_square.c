bd5f63691725cab4f2844535d885cdb65f0d1c9b
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-08 19:37:42 +0000

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,6 +11,8 @@ void print_square(int size)
 int i;
 int j;
 
+if (size > 0)
+{
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
@@ -20,4 +22,5 @@ _putchar('#');
 }
 
 _putchar('\n');
+}
 }
