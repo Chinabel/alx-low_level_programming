aecaed53801b69b75c6916066bee638cb5b1c9d3
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-08 02:16:41 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * followed by a new line
+ * @n: the integer for this function
+ * Return: 0 if success
+ */
+
+void print_numbers(void)
+{
+int n;
+
+if (n = 0; n <= 9; n++)
+{
+_putchar(n);
+}
+
+_putchar('\n');
+
+return (0);
+}
