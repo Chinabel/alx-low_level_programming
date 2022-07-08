d8261d1cdba2736fa5d83d8c83587aac9ebfd05b
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-08 02:31:23 +0000

    printing numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,15 @@
 /**
  * print_numbers - prints numbers from 0 to 9
  * followed by a new line
- * @n: the integer for this function
+ * @i: the integer for this function
  * Return: 0 if success
  */
 
 void print_numbers(void)
 {
-char i;
+char i = 0;
 
-for (i = 0; i < 10; i++)
+while (i < 10)
 {
 _putchar(i);
 i++;
