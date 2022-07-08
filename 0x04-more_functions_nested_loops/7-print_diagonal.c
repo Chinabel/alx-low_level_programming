f161e33e2456ae91859b9d14fa1a83ac0c65f492
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-08 18:34:37 +0000

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,8 +3,6 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: the number of lines to print
- * @x: the integer for the first axis
- * @y: the integer for the last axis
  * Return: 0
  */
 
@@ -17,6 +15,7 @@ if (n <= 0)
 {
 _putchar('\n');
 }
+
 else
 {
 for (x = 0; x < n; x++)
