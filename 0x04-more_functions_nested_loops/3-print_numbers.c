292004abdf2b530999500c1a8c51c4080764b838
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-08 02:39:51 +0000

    printing numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,13 +8,15 @@
 
 void print_numbers(void)
 {
-char i = 0;
+char n = 0;
 
-while (i <= 9)
+while (n <= 9)
 {
-_putchar('0' + i);
-i++;
+_putchar('0' + n);
+n++;
 }
 
 _putchar('\n');
+
+return (0);
 }
