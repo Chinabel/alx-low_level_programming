90f2cd84b7c44064819ff67c450e4900708291e9
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-09 01:54:07 +0000

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,16 +13,18 @@ int y;
 
 if (size > 0)
 {
-for (x = 1; x < size; x++)
+_putchar('\n');
+}
+else
 {
-for (y = 1; y < x; y++)
+for (x = 1; x < size; ++x)
 {
+for (y = 1; y < x; ++y)
+{
+_putchar(' ');
+}
 _putchar('#');
 _putchar('\n');
 }
-}
-else
-
-_putchar('\n');
 }
 }
