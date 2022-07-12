f38b0e4bbf1dbfb594c6d37edffeefbf4d859b5f
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-12 15:18:09 +0000

    print string in reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,33 @@
 
 void print_rev(char *s)
 {
-int c, len, temp;
+int c, len;
+char begin, end, temp;
+
 len = strlen(s);
+begin = s;
+end = s;
+
+for (c = 0; c <= len - 1; c++)
+end++;
 
 for (c = 0; c <= len/2; c++)
 {
-temp = s[c];
-s[c] = s[len - c - 1];
-s[len - c - 1] = temp;
+temp = *end;
+*end = *begin;
+*begin = *temp;
+
+begin++;
+end--;
 }
+}
+
+int strlen(char *p)
+{
+int c = 0;
+
+while (*(p + c) != '\0')
+c++;
 
 return (c);
 }
