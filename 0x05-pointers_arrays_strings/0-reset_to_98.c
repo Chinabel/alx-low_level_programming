225e909956e18bdbb923e63d2e3d92b7c79ff731
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-12 11:31:05 +0000

    print to pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,13 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to 98
- *
+ * @n: the integer to be used
  * Return: 0
  */
 
 void reset_to_98(int *n)
 {
-n = 402
+n = 402;
 
 _putchar(n);
 
