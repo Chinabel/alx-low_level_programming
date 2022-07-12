b98268170855046b8af4a94971cfa5db0377d247
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-12 20:57:44 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+char s[1000], r[1000];
+int begin, end, count = 0;
+
+while (s[count] != '\0')
+count++;
+
+end = count - 1;
+
+for (begin = 0; begin < count; begin++)
+{
+r[begin] = s[end];
+end--;
+}
+
+r[begin] = '\0';
+
+putchar(r);
+}
