6fe4c6fb8575210015057b767fa80c8c3e27c47e
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-12 13:00:59 +0000

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+char *s[7] = "School";
+int len = strlen(*s);
+}
