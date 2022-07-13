343a1bfa0f258f2e2e1947303744bf91942c6dc9
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-13 00:14:54 +0000

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: indicates a string
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+int str_size = strlen(str);
+int s, part_size;
+
+if (str_size % n != 0)
+{
+return;
+}
+
+part_size = str_size / n;
+
+for (s = 0; s < str_size; s++)
+{
+if (s % part_size == 0)
+putchar(str[n];
+}
+
+putchar('\n');
+}
