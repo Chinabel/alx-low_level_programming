4234dc4d6a36891c1e3e98e00492be1948217889
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-13 00:00:25 +0000

    print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 /**
- * puts2 - prints every other character of a string,starting with the first character
+ * puts2 - prints every other character of a string,
+ * starting with the first character
  * followed by a new line
  * @str: indicates string
  */
