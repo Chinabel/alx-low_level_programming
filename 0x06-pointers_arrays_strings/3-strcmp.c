adb1b55432a888232611c18eef5188213bc6372c
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-14 18:16:07 +0000

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,8 +12,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int result;
+int i;
 
-result = strcmp(s1, s2)
-return (result);
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+}
+
+return (0);
 }
