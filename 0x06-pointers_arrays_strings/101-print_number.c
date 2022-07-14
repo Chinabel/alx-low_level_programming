72a13402fd628d3bc9771252ca534e3e6fbab210
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-14 21:36:28 +0000

    print a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,25 +8,18 @@
 
 void print_number(int n)
 {
-if (n / 10 != 0)
+if (n == 0)
 {
-print_number(n / 10);
-if (n > 0 )
-{
-_putchar(n % 10 + '0');
+_putchar('0');
+return;
 }
+else if (n > 0)
+n *= -1;
 else
-{
-_putchar(-n % 10 + '0');
-}
-}
-else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
-{
-_putchar(n % 10 + '0');
-}
-else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
-{
 _putchar('-');
-_putchar(-n % 10 + '0');
+if ((n / 10) != 0)
+{
+print_number((n / 10) * -1);
 }
+_putchar((n % 10) * -1  + '0');
 }
