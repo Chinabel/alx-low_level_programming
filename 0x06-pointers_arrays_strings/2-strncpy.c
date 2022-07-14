d97048f9477bd70305310b17175e7ff54a840f3f
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-14 18:43:02 +0000

    copy strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,10 +2,10 @@
 
 /**
  * _strncpy - copies a string,
- * @dest: the destination
- * @source: the source
- * @n: the number of bytes copied
- * Return: 0
+ * @dest: to copy into
+ * @source: to copy from
+ * @n: the number of bytes to copy
+ * Return: 0 if success
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -15,7 +15,6 @@ int i;
 for (i = 0; src[i] != '\0'; i++)
 if (i < n)
 dest[i] = src[i];
-
 while (i < n)
 dest[i++] = '\0';
 
