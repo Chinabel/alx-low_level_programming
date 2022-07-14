4e910ab409558cc3cfb9d963ed9ccd8687099d12
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-14 21:11:49 +0000

    print a number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: an integer
+ * Return: 0 if success
+ */
+
+void print_number(int n)
+{
+if (n / 10 != 0)
+{
+print_number(n / 10);
+if (n > 0 )
+{
+_putchar(n % 10 + '0');
+}
+else
+{
+_putchar(-n % 10 + '0');
+}
+}
+else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+{
+_putchar(n % 10 + '0');
+}
+else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+{
+_putchar('-');
+_putchar(-n % 10 + '0');
+}    
+}
