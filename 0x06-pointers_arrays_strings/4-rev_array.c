69d5e636f6eab1e2fed9dd69a1cb0feeb9db7136
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-14 18:58:20 +0000

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,11 @@ void reverse_array(int *a, int n)
 int i;
 int temp;
 
-for (i = 0; i < n / 2; i++)
+for (i = 0; i < n; i++)
 {
+n--;
 int temp = a[i];
 a[i] = a[n - 1 - i];
 a[n - 1 - i] = temp;
 }
-
-return (0);
 }
