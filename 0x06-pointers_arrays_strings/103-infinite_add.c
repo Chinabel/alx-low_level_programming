70809e413f0b35e44508101b6fa3a23ca6034ea0
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-14 22:14:00 +0000

    add numbers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: the first number
+ * @n2: the second number
+ * @r: the buffer to store the result
+ * @size_r: the buffer size
+ * Return: the pointer to the result else 0
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+r = size_r(n1, n2);
+return (r);
+
+if (r != size_r)
+
+return (0);
+}
