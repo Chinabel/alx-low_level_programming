7b04b770dae3aa5316da8eee1b5b2eda08c0ee10
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-14 20:31:25 +0000

    capitalized string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,16 +3,32 @@
 /**
  * cap_string - capitalises all words of a string
  * @s: input string
- * Return: capitalized string
+ * Return: first letter capitalize after separator
  */
 char *cap_string(char *s)
 {
-int i;
+int i, j;
+int cap = 32;
+int separators[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
 
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] >= 'a' && s[i] <= 'z')
-s[i] = s[i] - 32;
+{
+s[i] = s[i] - cap;
 }
+
+cap = 0;
+
+for (j = 0; j <= 12; j++)
+{
+if (s[i] == separators[j])
+{
+j = 12;
+cap = 32;
+}
+}
+}
+
 return (s);
 }
