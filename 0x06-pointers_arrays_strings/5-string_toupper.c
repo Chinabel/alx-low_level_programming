d8ed2f236a951bd981526994fa126e12393e14bd
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-15 04:16:31 -0700

    change to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,15 @@
 
 char *string_toupper(char *s)
 {
-int i;
+	int i;
 
-i = 0;
+	i = 0;
 
-while (*(s + i) != '\0')
-{
-if ((*(s + i) >= 'a') && (*(s + i) <= 'z'))
-*(s + i) = *(s + i) - 32;
-i++;
-}
-return (s);
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] -32;
+		i++;
+	}
+	return (s);
 }
