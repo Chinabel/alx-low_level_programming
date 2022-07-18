9f439bebcba50874e31c1023087eb33d83f3b1ed
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-18 17:16:16 +0000

    set string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * set_string - sets the value of a pointer to a char.
  * @s: the pointer to modify
- * @to: the value to assign to
+ * @to: the value to assign
  * Return: 0
  */
 
