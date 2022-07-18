afc8c724d88dad156391b67f8f4e7eaf1b185779
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-18 15:50:13 +0000

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * print_chessboard - prints the chessboard.
+ * print_chessboard - prints the chessboard
  * @a: the row of the  array
  */
 
@@ -13,6 +14,7 @@ for (i = 0; a[i][7]; i++)
 {
 for (j = 0; j < 8; j++)
 _putchar(a[i][j]);
+
 _putchar('\n');
 }
 } 
