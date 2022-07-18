499e6280f5acefc8ab4016783f558143e4b890bf
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-18 14:54:09 +0000

    locate substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,11 +2,13 @@
 #include <string.h>
 
 /**
- * _strstr - locates a substring.
+ * _strstr - locates a substring
  * @haystack: a string
  * @needle: a substring
- * Return: a pointer to the beginning of the located substring
- * or NULL if not found
+ * Return:
+ * 	a pointer to the beginning of the located substring
+ * 	or NULL if not found
+ *
  */
 
 char *_strstr(char *haystack, char *needle)
