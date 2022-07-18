585efe5f593fbe46c4bdb0959f4952fe9406c0ef
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-18 15:39:00 +0000

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,6 @@
 /**
  * print_chessboard - prints the chessboard.
  * @a: the row of the  array
- * Return: nothing
  */
 
 void print_chessboard(char (*a)[8])
@@ -11,7 +10,6 @@ void print_chessboard(char (*a)[8])
 int x, y;
 
 x = y = 0;
-
 while (x < 8)
 {
 y = 0;
