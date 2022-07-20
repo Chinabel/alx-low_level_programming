2b62ccabfbb6aa8c2c2d95adb5565cbf900cdc27
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-20 05:52:56 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0'")
+		return;
+	puts("%c", s);
+	s++;
+}
