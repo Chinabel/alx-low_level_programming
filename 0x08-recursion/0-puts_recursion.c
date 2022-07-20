ea96f2fa528f9217f0c72c48a3f3762017f1194c
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-20 06:06:17 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,8 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0'")
-		return;
-	puts("%c", s);
-	s++;
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
