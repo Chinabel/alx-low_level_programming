97e370b5ca685391a1cd937828e884d179629886
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-20 07:08:22 -0700

    print reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,10 +7,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i;
-		if (*(s + i) == '\0')
+	if (*s)
 		{
-			_print_rev_recursion(*(s + i) + 1);
-			_putchar(*(s + i));
+			_print_rev_recursion(s + 1);
+			_putchar(*s);
 		}
 }
