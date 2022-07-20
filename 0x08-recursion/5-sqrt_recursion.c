3110ab04a4a0ce29f6563808e90d4b7bcdb50dee
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-20 21:03:01 +0000

    find squareroot

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,42 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to use
- * Return: _sqrt_recursion()
- */
+ * sqtChecker - computes square root recursively doing binary search
+ * @min: inital number
+ * @max: last number within the limit of number
+ * @m: given number
+ * Return: 1 if not found sqrroot, else sqrroot
+ **/
 
-int _sqrt_recursion(int n)
+int sqtChecker(int min, int max, int m)
 {
-if (n == 0 || n == 1)
-return (n);
-return (_sqrt(0, n));
+long int guess;
+
+if (max >= min)
+{
+guess = min + (max - min) / 2;
+if (guess * guess == m)
+return (guess);
+/* following binary search */
+if (guess * guess > m)
+return (sqtChecker(min, guess - 1, m));
+if (guess * guess < m)
+return (sqtChecker(guess + 1, max, m));
+}
+return (-1);
 }
 
 /**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @i: squared number
- *
- * Return: _sqrt()
- */
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ **/
 
-int _sqrt(int n, int i)
+int _sqrt_recursion(int n)
 {
-if (n > i / 2)
+if (n < 0)
 return (-1);
-else if (n * n == i)
-return (n);
-return (_sqrt(n + 1, i));
+if (n == 0)
+return (0);
+return (sqtChecker(1, n, n));
 }
