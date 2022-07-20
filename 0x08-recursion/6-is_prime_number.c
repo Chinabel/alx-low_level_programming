c42f851a753b2a70b4eacd92153ad16b13bca3e0
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-20 21:30:41 +0000

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * find_multipliers - look for multipliers of n.
+ * @n: base number.
+ * @m: iterator number.
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+
+int find_multipliers(int n, int m)
+{
+if (m == n)
+return (1);
+if (n % m == 0)
+return (0);
+else
+return (find_multipliers(n, m + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: the integer to use
+ * Return: 0 else
+ */
+
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+
+return(primenumber(n, n / 2);
+}
