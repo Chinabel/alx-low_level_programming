36a08435fbb6d9a6314cdfca7b342e408ce817b0
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-21 04:08:27 +0000

    compare for identical strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,22 +2,24 @@
 #include <string.h>
 
 /**
- * str_identifier - check if two strings are identical
+ * str_idn - check if two strings are identical
  * @s1: string_1 base address
  * @s2: string_2 base address
+ * @a: the first index
+ * @b: the second index
  * Return: 1 if s is palindrome, 0 otherwise
  */
 
-int str_identifier(char *s1, char *s2)
+int str_idn(char *s1, char *s2, int a, int b)
 {
-if (*s1 == '\0' && *s2 == '\0')
+if (s1[a] == '\0' && s2[b] == '\0')
 return (1);
-if (*s1 == *s2)
-return (str_identifier(s1, s2, i + 1, j + 1));
-if (*s1 == '\0' && *s2 == '*')
-return (str_identifier(s1, s2, i, j + 1));
-if (*s2 == '*')
-return (str_identifier(s1, s2, i + 1, j) || str_identifier(s1, s2, i, j + 1));
+if (s1[a] == s2[b])
+return (str_idn(s1, s2, a + 1, b + 1));
+if (s1[a] == '\0' && s2[b] == '*')
+return (str_idn(s1, s2, a, b + 1));
+if (s2[b] == '*')
+return (str_idn(s1, s2, a + b, b) || str_idn(s1, s2, a, b + 1));
 return (0);
 }
 
@@ -37,7 +39,7 @@ if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
 
 else if (*s2 == '*')
-return (str_identifier(s1, (s2 + 1), (s2 + 1)));
+return (str_idn(s1, (s2 + 1), (s2 + 1)));
 
 else
 return (0);
