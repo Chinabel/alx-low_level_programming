5344303589f7f4cb30a4a219618b08cf82d10eeb
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-21 04:00:26 +0000

    compare for identical strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,6 +2,26 @@
 #include <string.h>
 
 /**
+ * str_identifier - check if two strings are identical
+ * @s1: string_1 base address
+ * @s2: string_2 base address
+ * Return: 1 if s is palindrome, 0 otherwise
+ */
+
+int str_identifier(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (str_identifier(s1, s2, i + 1, j + 1));
+if (*s1 == '\0' && *s2 == '*')
+return (str_identifier(s1, s2, i, j + 1));
+if (*s2 == '*')
+return (str_identifier(s1, s2, i + 1, j) || str_identifier(s1, s2, i, j + 1));
+return (0);
+}
+
+/**
  * wildcmp - compares two strings, returns 1 if the strings are identical
  * @s1: the first string value
  * @s2: the second string value
@@ -10,20 +30,14 @@
 
 int wildcmp(char *s1, char *s2)
 {
-int i = 0, n1 = strlen(s1), n2 = strlen(s2), b = 0;
-
-if (n1 != n2)
-{
-return (0);
-}
-else if (i <= n1)
-{
-b++;
-i++;
-wildcmp(s1, s2);
-}
-if (b == i)
+if (*s1 == '\0' && *s2 == '\0')
 return (1);
+
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+
+else if (*s2 == '*')
+return (str_identifier(s1, (s2 + 1), (s2 + 1)));
 
 else
 return (0);
