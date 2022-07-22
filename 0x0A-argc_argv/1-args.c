ca0e6d6c1b1e0b63f1ccfca052d215e7ba349711
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-22 14:45:11 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: the argument count
+ * @argv: the arrau the argument is pointing to
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < agrc; i++)
+	{
+		printf("%s", agrv[i]);
+	}
+
+	return (0);
+}
