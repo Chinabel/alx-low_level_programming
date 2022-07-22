926d2f34c0391786607042477bbbc8913b1b23be
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-22 12:55:50 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: the argument count
+ * @argv: the array argument index
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+
+	printf('\n');
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+An argc argv readme file
