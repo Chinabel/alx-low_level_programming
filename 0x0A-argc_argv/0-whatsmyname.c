68594b9f01c09843c27d206affd57673b3e5d6e9
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-22 13:11:28 -0700

    print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,16 +5,11 @@
  * main - prints its name, followed by a new line.
  * @argc: the argument count
  * @argv: the array argument index
+ * Return: 0
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s ", argv[i]);
-	}
-
-	printf('\n');
+	print("Program name is: %s\n", argv[0]);
+	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 int isInteger(const char *s);
 
 #endif
