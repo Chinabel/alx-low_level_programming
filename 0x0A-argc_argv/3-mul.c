b0c45dd32892ff23e65502220791126b910ebd86
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-22 17:37:01 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ *
+ * @argc: the argument count
+ * @argv: the array containing the arguments
+ * Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int product = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		product = product * argv[i];
+	}
+
+	printf("%d\n", product);
+	return (0);
+}
