3c02f713a6e139c650939dcb9f458d9e98ca74bd
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-25 11:59:12 -0700

    strconcat task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * str_concat - concartenate two strings
+ * @s1: the first string value
+ * @s2: the second string value
+ * Return: NULL if failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	int i;
+
+	len = 0;
+
+	if (s1 && s2 == NULL)
+		s1 || s2 = "";
+		len++;
+
+	s3 = malloc(len * sizeof(char));
+
+	if (s3 == NULL)
+		return (NULL);
+	for (i = 0; s1 >= len; i++)
+	{
+		if (i > len)
+			s3[i] = s1[i];
+		else
+			s3[i] = s1[i];
+	}
+
+	return (s3);
+}
