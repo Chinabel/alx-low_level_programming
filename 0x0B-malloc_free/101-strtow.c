cc395bb5a17e133845b68cc20032ce681079c83f
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-25 17:19:51 -0700

    strtow task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,7 +9,26 @@
 
 char **strtow(char *str)
 {
-	int n;
+	int n1 = 0, n2 = 0, size = word_count(str);
+	char **ch;
+
+	ch = malloc(size * sizeof(char *));
 
 	if (str == NULL || str == "")
 		return (NULL);
+	while (str[n1])
+	{
+		if (str[n1] == ' ')
+		{
+			n1++;
+			n2++;
+		}
+		else
+			ch[0] = malloc(0 * sizeof(char));
+			ch[0] = &str[0];
+		n++;
+	}
+	**ch = ' ';
+
+	return (ch);
+}
