83c8d6b8d7ff8eb91a8eaade98edf79594adc3a9
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-25 18:26:18 -0700

    strtow task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,38 +2,65 @@
 #include <string.h>
 
 /**
- * strtow - splits a string into words
- * @str: a string
- * Return: a pointer to an areay of strings
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ **/
+
+void ch_free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
+
+/**
+ * strtow - splits a string into words.
+ * @str: the string value
+ * Return: pointer of an array of integers
  */
 
 char **strtow(char *str)
 {
-	int n1 = 0, n2 = 0, size, count;
 	char **ch;
+	int i, height, a, b, k;
 
-	size = count(str);
-
-	ch = malloc(size * sizeof(char *));
-
-	if (str == NULL || str == " ")
+	if (str == NULL || *str == '\0')
 		return (NULL);
+	for (i = height = 0; str[i] != '\0'; i++)
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			height++;
 
-	while (str[n1])
+	ch = malloc((height + 1) * sizeof(char *));
+	if (ch == NULL || height == 0)
 	{
-		if (str[n1] == ' ')
-		{
-			n1++;
-			n2++;
-		}
-		else
-		{
-			ch[n2] = malloc(n1 * sizeof(char));
-			ch[n2] = &str[n1];
-		}
-		n1++;
+		free(ch);
+		return (NULL);
 	}
-	**ch = ' ';
-
+	for (a = k = 0; a < height; a++)
+	{
+		for (i = k; str[i] != '\0'; i++)
+		{
+			if (str[i] == ' ')
+				k++;
+			if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			{
+				ch[a] = malloc((i - k + 2) * sizeof(char));
+				if (ch[a] == NULL)
+				{
+					ch_free_grid(ch, a);
+					return (NULL);
+				}
+				break;
+			}
+		}
+		for (b = 0; k <= i; k++, b++)
+			ch[a][b] = str[k];
+		ch[a][b] = '\0';
+	}
+	ch[a] = NULL;
 	return (ch);
 }
