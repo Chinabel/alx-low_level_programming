2de2af25397a4cd84d3e959d92d1edd23c68827d
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-28 08:44:10 -0700

    concartenate stringgs

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,13 +11,21 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
-	int i, j = 0;
+	size_t a, b, c, n = 0;
 
-	s3 = malloc(strlen(s1) + strlen(s2) + 1);
-	for (i = 0; (s3[j] = s1[i]) != '\0'; i++, j++)
-	{}
-	for (i = 0; (s3[j] = s2[i]) != '\0'; i++, j++)
-	{}
+	a = strlen(s1);
+	b = strlen(s2);
+	c = a + b + 1;
+
+	s3 = malloc(c);
+
+	if (s3 == NULL)
+		return (NULL)
+
+	if (n >= b)
+		return (b)
+
+	s3[n] = '\0';
 
 	return (s3);
 }
