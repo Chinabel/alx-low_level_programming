2f36a1cac43a471ee59435fda55db9890a02eaa5
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-28 08:08:18 -0700

    checking malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: the integer
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *str;
+
+	str = malloc(b * sizeof(str));
+
+	if (str == NULL)
+		exit(98);
+
+	return (b);
+}
