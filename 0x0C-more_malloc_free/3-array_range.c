a92e59114217df77924aa413dfdd11a6ca4c3619
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-28 20:09:33 -0700

    create array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; n = min; n <= max; i++, n++)
+	for (i = 0, n = min; n <= max; i++, n++)
 		arr[i] = n;
 
 	return (arr);
