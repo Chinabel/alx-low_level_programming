a2016ecf5b25d114fc6609ceb50f3677736cb552
Chineze Ogugua <Christabelogugua@gmail.com>
2022-07-28 20:28:47 -0700

    reallocate memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		str = malloc(new_size);
 		if (str == NULL)
-			return (NULL)
+			return (NULL);
 		return (str);
 	}
 	if (new_size == 0 && ptr != NULL)
