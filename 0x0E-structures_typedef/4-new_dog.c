78a95b57bb9a894f5e9e782f537b781124ca1557
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-01 14:50:53 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,30 +17,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	while (name[i] != '\0')
 		i++;
-	while (name[j] != '\0')
+	while (owner[j] != '\0')
 		j++;
 	doge = malloc(sizeof(dog_t));
 	if (doge == NULL)
 	{
-		return (NULL);
-	}
-	(*doge).name = malloc(i * sizeof((*doge).name));
-	if ((*doge).name == NULL)
-	{
 		free(doge);
 		return (NULL);
 	}
-	for (k = 0; k <= i; i++)
-		(*doge).name[k] = name[k];
-	(*doge).age = age;
-	(*doge).owner = malloc(j * sizeof((*doge).owner));
-	if ((*doge).owner == NULL)
+	doge->name = malloc(i * sizeof(doge->name));
+	if (doge->name == NULL)
 	{
-		free((*doge).name);
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		doge->name[k] = name[k];
+	doge->age = age;
+	doge->owner = malloc(j * sizeof(doge->owner));
+	if (doge->owner == NULL)
+	{
+		free(doge->owner);
+		free(doge->name);
 		free(doge);
 		return (NULL);
 	}
 	for (k = 0; k <= j; k++)
-		(*doge).owner[k] = owner[k];
+		doge->owner[k] = owner[k];
 	return (doge);
 }
