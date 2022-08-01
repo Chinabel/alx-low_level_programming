17b335f00a5917c31500f147dca6c47110f8cdd5
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-01 06:08:19 -0700

    struct type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
