4adc325837590aff1176bd943e104d7cccb3d9a9
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-01 13:59:10 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,7 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (k = 0; k <= i; i++)
-		(*doge).name[k] = name[k];
+		doge->name[k] = name[k];
 	(*doge).age = age;
 	(*doge).owner = malloc(j * sizeof((*doge).owner));
 	if ((*doge).owner == NULL)
@@ -44,6 +44,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (k = 0; k <= j; k++)
-		(*doge).owner[k] = owner[k];
+		doge->owner[k] = owner[k];
 	return (doge);
 }
