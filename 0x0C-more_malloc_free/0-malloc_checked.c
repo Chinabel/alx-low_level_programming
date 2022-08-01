f43f516f3e093deb7588a7e64f4f880c725884c9
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-01 15:37:30 -0700

    check malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocates dynamic memory
- * @b: the integer to indicate the size of memory
+ * @b: the size of memory
  * Return: pointer to allocated memory
- **/
+ */
 
 void *malloc_checked(unsigned int b)
 {
