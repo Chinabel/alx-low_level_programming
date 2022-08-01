c12cd2e366cae863e65f2e05efeeb45cb6fab0ac
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-01 07:27:14 -0700

    print struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,23 +13,16 @@ void print_dog(struct dog *d)
 	{
 		if ((*d).name == NULL)
 		{
-			printf(nil);
+			printf("Name: (nil)\n";
 		}
 		else
 		{
 			printf("Name: %s\n", (*d).name);
 		}
-		if ((*d).age == NULL)
-		{
-			printf(nil);
-		}
-		else
-		{
-			printf("Age: %f\n", (*d).age);
-		}
+		printf("Age: %f\n", (*d).age);
 		if ((*d).owner == NULL)
 		{
-			printf(nil);
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
