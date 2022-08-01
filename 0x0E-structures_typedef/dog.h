3da04c88d1ce7b0cec83f1c0a092a453e4180871
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-01 06:39:33 -0700

    struct heade

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog {
+struct dog {
 	char *name;
-	char *owner;
 	float age;
-} dog;
+	char *owner;
+};
 
+typedef struct dog my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
