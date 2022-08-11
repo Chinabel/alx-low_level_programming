486b75bd549e7372e7d6ac1d2bd4ed1544ffde9e
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-11 13:11:53 -0700

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,17 +10,36 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *lnode;
+	list_t *lnode, *temp;
+	int lenst;
 
-	if (*head == NULL)
-		return (NULL);
-
+	for (lenst = 0; str[lenst] != '\0'; lenst++)
+		;
 	lnode = malloc(sizeof(list_t));
+	temp = *head;
+	if (lnode == NULL)
+	{
+		return (NULL);
+		free(lnode);
+	}
+
+	lnode->len = lenst;
 	lnode->str = strdup(str);
 	lnode->next = NULL;
-	while (*head->next != NULL)
-		*head = *head->next;
 
-	*head->next = lnode;
-	return (lnode);
+	if (*head == NULL)
+	{
+		*head = lnode;
+		return (*head);
+	}
+	else
+	{
+		while (*temp->next != 0)
+		{
+			temp = temp->next;
+		}
+	}
+	temp->next = lnode;
+	return (*head);
+	free(lnode);
 }
