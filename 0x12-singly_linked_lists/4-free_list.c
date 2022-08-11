8f4df1f37bc563b09ba72d09424d898a317350b5
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-11 13:24:29 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: the node identifier
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while (head != NULL)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
