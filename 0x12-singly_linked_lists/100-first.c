8054762b544b3eb7af330f268d41960d6478eecc
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-11 13:39:57 -0700

    print lines

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * main - prints a line followed by a new line
+ * before main function is executed
+ * Return: 0 if success
+ */
+
+void __attribute__ ((constructor)) main()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
