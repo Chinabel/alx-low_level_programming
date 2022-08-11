57424bd9c802caa1646379e1b23db56c7f6650a7
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-11 13:42:52 -0700

    print new lines

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * main - prints a line followed by a new line
+ * bmain - prints a line followed by a new line
  * before main function is executed
  * Return: 0 if success
  */
 
-void __attribute__ ((constructor)) main()
+void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
