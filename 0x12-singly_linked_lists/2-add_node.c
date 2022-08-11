3d8631d498b7146acf71c8ea3740b36919e46a3c
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-11 12:31:55 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,20 +9,23 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *fnode;
-	size_t nchar;
+	int lenst;
+
+	for (lenst = 0; str[lenst] != '\0'; lenst++)
+		;
 
 	fnode = malloc(sizeof(list_t));
 	if (fnode == NULL)
+	{
 		return (NULL);
+		free(fnode);
+	}
 
+	fnode->len = lenst;
 	fnode->str = strdup(str);
-
-	for (nchar = 0; str[nchar]; nchar++)
-		;
-
-	fnode->len = nchar;
 	fnode->next = *head;
 	*head = fnode;
 
-	return (*head);
+	return (fnode);
+	free(fnode);
 }
