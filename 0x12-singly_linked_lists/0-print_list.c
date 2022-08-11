ecfe4cb816783e6f4231a97d7d7d5af2fc76ba25
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-11 07:44:59 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,12 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if ((*h).str == NULL)
+		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf("[%d] %s\n", (*h).len, (*h).str);
-		h = (*h).next;
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		numel++;
 	}
 	return (numel);
 }
