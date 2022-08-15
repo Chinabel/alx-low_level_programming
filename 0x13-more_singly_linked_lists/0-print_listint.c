2a83dde487ed63e1fb3f67d715d162a880ced5e2
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-15 14:46:38 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t temp;
+	size_t temp = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%d ", h->n);
 		h = h->next;
 		temp++;
 	}
-
+	printf("\n");
 	return (temp);
 }
