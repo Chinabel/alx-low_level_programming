436b4395b06d7c66a27e237fc02a8ef562f6a3d1
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-15 15:43:26 -0700

    add element end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,21 +8,25 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newel = malloc(sizeof(listint_t));
-	listint_t *lnode = (*head);
+	listint_t *newel;
+	listint_t *lnode;
 	(void)lnode;
 
+	newel = malloc(sizeof(listint_t));
 	if (newel == NULL)
 		return (NULL);
 	newel->n = n;
 	newel->next = NULL;
+	lnode = (*head);
 	if ((*head) == NULL)
 	{
 		(*head) = newel;
 		return;
 	}
 	while (lnode->next != NULL)
+	{
 		lnode = lnode->next;
+	}
 
 	lnode->next = newel;
 	return (*head);
