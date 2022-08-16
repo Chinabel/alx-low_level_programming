d4bbbd34414d21ce92dab52c6dfaa569a95b0ca5
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-16 05:59:59 -0700

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,8 +10,8 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int size;
-	listint_t *temp;
 	listint_t *nnode;
+	listint_t *temp;
 
 	temp = *head;
 	if (idx != 0)
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		nnode->next = *head;
-		*head = new;
+		*head = nnode;
 	}
 	else
 	{
