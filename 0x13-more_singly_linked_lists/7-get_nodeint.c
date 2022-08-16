88e3bf2f8fc5871e1d6ac810013ce08020523166
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-16 02:28:25 -0700

    nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,13 +9,11 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-	listint_t *current;
 
 	current = head;
 	while (current != NULL)
 	{
 		if (count == index)
-			return (current->n);
 		count++;
 		current = current->next;
 	}
