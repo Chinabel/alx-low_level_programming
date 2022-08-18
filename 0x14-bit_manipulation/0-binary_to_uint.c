9081a66c6516abfbc1cbb3c7c938a45a69421f34
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-18 08:49:12 -0700

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,21 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int i = 1;
 	unsigned int n = 0;
-	int i = 0;
+	int c;
+	unsigned int len;
 
-	if (b == NULL)
-		return (0);
-
-	while (b[i] == '0' || b[i] == '1')
+	len = strlen(b);
+	for (c = len - 1; c >= 0; c--)
 	{
-		n <<= 1;
-		n += b[i] - '0';
-		i++;
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			n += i;
+		}
+		i *= 2;
 	}
 	return (n);
 }
