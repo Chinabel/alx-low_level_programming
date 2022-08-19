19ce049d1156a613ef727723eb9a16df203a6791
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-19 04:50:20 -0700

    bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 int _putchar(char *c);
 unsigned int binary_to_uint(const char *b);
+int check_valid_string(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
