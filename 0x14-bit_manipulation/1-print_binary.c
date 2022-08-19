dd1d4422e8879932782bbfe15c052b49c29f131a
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-19 05:14:24 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,21 +7,19 @@
 
 void print_binary(unsigned long int n)
 {
-	int b, count = 0;
-	unsigned long int current;
+	char varChr_binary;
 
-	for (b = 63; b >= 0; b--)
+	if (n == 0)
 	{
-		current = n >> b;
-
-		if (current & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
-	}
-	if (!count)
 		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	print_binary(n >> 1);
+	varChr_binary = (n & 1) ? '1' : '0';
+	_putchar(varChr_binary);
 }
