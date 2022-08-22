948e247e2afdeca5f24ec27a25b3fd4f73f43b03
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-22 08:39:01 -0700

    task 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,28 +9,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, r_write, len;
+	int fd, r_write, num;
 
 	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
 	{
-		fd = open(filename, O_WRONLY | O_APPEND);
-		if (fd == -1)
-			return (-1);
-		len = 0;
-		while (*(text_context + len) != '\0')
-			len++;
-
-		r_write = write(fd, text_content, len);
+		for (num = 0; textcontent[num]; num++)
+			;
+		r_write = write(fd, text_content, num);
 		if (r_write == -1)
-		{
-			close(fd);
-			write(STDOUT_FILENO, "fails", 5);
 			return (-1);
-		}
 	}
+
 	close(fd);
 	return (1);
 }
