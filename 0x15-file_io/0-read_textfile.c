67ffa2700b58a622ea29b563d62702332c545bcc
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-22 07:58:24 -0700

    task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,17 +18,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	x = open(filename, O_RDONLY);
+
 	if (x == -1)
 		return (0);
 
 	out = malloc(sizeof(char) * (letters));
+
 	if (!out)
 		return (0);
 
 	frd = read(x, out, letters);
 	if (frd == -1)
 		return (0);
-
 	fwr = write(STDOUT_FILENO, out, frd);
 
 	free(out);
