14afbf98e76873a171c542f494b3e1b374975c4f
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-22 08:05:25 -0700

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,13 +4,12 @@
  * create_file - creates a file
  * @filename: the name of file to be created
  * @text_content:a NULL terminated string
- * Return: 1 if success
+ * Return: 1 if success, -1 on failure or if filename is NULL
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr;
-	int num = 0;
+	int op, wr, num = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		for (num = 0; textcontent[num];)
 			num;
 	}
-	op = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, num);
 	if (op == -1 || wr == -1)
 		return (-1);
