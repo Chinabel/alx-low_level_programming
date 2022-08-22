9e4e78c1f9d7f50c8c204b1e3e82b536346c8e81
Chineze Ogugua <Christabelogugua@gmail.com>
2022-08-22 08:16:06 -0700

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,20 +11,22 @@ int create_file(const char *filename, char *text_content)
 {
 	int op, wr, num = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (num = 0; text_content[num];)
-			num;
-	}
-	
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (op == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+	for (num = 0; text_content[num]; num++)
+		;
+
 	wr = write(op, text_content, num);
-	
-	if (op == -1 || wr == -1)
+
+	if (wr == -1)
 		return (-1);
 
 	close(op);
