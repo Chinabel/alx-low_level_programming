2489e09eef2f4a896f2b5dd6a4566fb892e346ca
Chineze Ogugua <christabelogugua@gmail.com>
2022-09-30 08:22:52 -0700

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,19 +1,47 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * free_dlistint - free a dlistint_t list
- * @head: head of linked list
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the beginning of the linked list
+ * @idx: index at which to insert the new node
+ * @n: data to enter into new node
+ * Return: pointer to the new node, or NULL on failure
  */
-
-void free_dlistint(dlistint_t *head)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *next;
+	dlistint_t *new, *next, *current;
+	unsigned int i;
 
-	while (head != NULL)
+	if (h == NULL)
+		return (NULL);
+	if (idx != 0)
 	{
-		next = head->next;
-		free(head);
-		head = next;
+		current = *h;
+		for (i = 0; i < idx - 1 && current != NULL; i++)
+			current = current->next;
+		if (current == NULL)
+			return (NULL);
 	}
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		next = *h;
+		*h = new;
+		new->prev = NULL;
+	}
+	else
+	{
+		new->prev = current;
+		next = current->next;
+		current->next = new;
+	}
+	new->next = next;
+	if (new->next != NULL)
+		new->next->prev = new;
+	return (new);
 }
